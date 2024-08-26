9cf9f5ab1c0df5b1427f7b1e29da8140a9827783
Diya Shajith K <diyashajith.2231@gmail.com>
2024-08-26 10:45:36 +0530

    Initial commit

---
+++ lib/view/getstarted.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class Getstarted extends StatefulWidget {
+  const Getstarted({super.key});
+
+  @override
+  State<Getstarted> createState() => _GetstartedState();
+}
+
+class _GetstartedState extends State<Getstarted> {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              width: size.width * 0.4,
+              height: size.height * 0.4,
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage(
+                        "assets/food-background.png",
+                      ),
+                      fit: BoxFit.contain)),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              "Shop Your Daily\nNecessary",
+              style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: 250,
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5)),
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    "Get Started",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500),
+                  )),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
